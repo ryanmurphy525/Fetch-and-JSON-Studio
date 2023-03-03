74e1698ab0531e214a737b2b09c8af75515f2b38
Ryan Murphy <ryanmurphy@RPM-MBP.local>
2023-03-02 19:51:36 -0600

    did the studio

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then( function(json){
+            console.log(json);
+            for (let index = 0; index < json.length; index++) {
+            let container = document.getElementById("container");
+            container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                    <h3>${json[index].firstName} ${json[index].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[index].hoursInSpace}</li>
+                        <li>Active: ${json[index].active}</li>
+                        <li>Skills: ${json[index].skills}</li>
+                    </ul>
+                    </div>
+                <img class="avatar" src=${json[index].picture}>
+                </div>`;
+            }
+        });
+    });
+});
